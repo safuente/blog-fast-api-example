e9bb2a0898f13d55cb33d45d39cb012a4767e0a6
Santiago Alvarez Fuente <saaf@MBP-de-Santiago.JOOWIN>
2023-07-13 15:47:22 +0200

    Starter with db configured

---
+++ backend/alembic/env.py
@@ -0,0 +1,5 @@
+from logging.config import fileConfig
+###
+from db.base import Base
+
+target_metadata = Base.metadata
---
+++ backend/alembic/versions/a9e18f2ef0f0_first_commit.py
@@ -0,0 +1,24 @@
+"""first commit
+
+Revision ID: a9e18f2ef0f0
+Revises: 
+Create Date: 2023-07-12 18:13:27.409395
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a9e18f2ef0f0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
